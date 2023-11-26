d95ea1080098f7c0429ce7f72eeeb77de5d7b95c
MichaelGloessl04 <gloess190117@gmail.com>
2023-11-26 15:41:45 +0100

    updated docstring

--- js/calendar-handler.js
+++ js/calendar-handler.js
@@ -5,9 +5,20 @@ module.exports = class CalendarHandler {
     #headers;
     #server_address;
     /**
-     * Interacts with the 'the events calendar' wordpress plugin api.
-     * @param {any} server_address should be a valid wp installation address.
-     * @param {any} headers access token.
+     * Creates a new instance of the CalendarHandler class.
+     *
+     * @constructor
+     * @param {string} server_address - The base server address for the WordPress Tribe Events API.
+     * @param {Object} headers - The headers to be included in the HTTP requests.
+     * @param {string} headers.authorization - The authorization token for authenticating requests.
+     *
+     * @example
+     * const serverAddress = 'https://example.com';
+     * const headers = {
+     *   authorization: 'Basic YOUR_ACCESS_TOKEN',
+     * };
+     *
+     * const calendarHandler = new CalendarHandler(serverAddress, headers);
      */
     constructor(server_address, headers) {
         this.#server_address = server_address + '/wordpress/wp-json/tribe/events/v1/';
@@ -15,10 +26,23 @@ module.exports = class CalendarHandler {
     }
 
     /**
-     * Return events from the databank.
-     * If id is left undefined then all events get returned.
-     * @param {any} id (optional) id of the desired event.
-     * @returns {array} list of events.
+     * Asynchronously retrieves events from the WordPress Tribe Events API based on the provided ID.
+     *
+     * @async
+     * @param {string} [id] - The ID of a specific event to retrieve. If not provided, retrieves all events.
+     * @returns {Promise<Array<Object>>} A promise that resolves to an array of events.
+     * @throws {Error} If an error occurs during the execution of the method.
+     *
+     * @example
+     * const calendarHandler = new CalendarHandler(serverAddress, headers);
+     *
+     * try {
+     *   const allEvents = await calendarHandler.get_events(); // Retrieves all events
+     *   const specificEvent = await calendarHandler.get_events('eventID'); // Retrieves a specific event
+     *   console.log(allEvents); // Array of events
+     * } catch (error) {
+     *   console.error(error.message);
+     * }
      */
     async get_events(id) {
         const endpoint = this.#server_address + 'events/';
@@ -37,8 +61,28 @@ module.exports = class CalendarHandler {
     }
 
     /**
-     * Add a new event to the database.
-     * @param {dict} payload should contain at least id keyword. all other properties are optional.
+     * Asynchronously creates a new event using the WordPress Tribe Events API.
+     *
+     * @async
+     * @param {Object} payload - The data payload for creating a new event.
+     * @returns {Promise<void>} A promise that resolves when the event creation is successful.
+     * @throws {Error} If an error occurs during the execution of the method.
+     *
+     * @example
+     * const calendarHandler = new CalendarHandler(serverAddress, headers);
+     * const eventPayload = {
+     *   title: 'New Event',
+     *   start_date: '2023-12-01',
+     *   end_date: '2023-12-02',
+     *   // Add other necessary properties
+     * };
+     *
+     * try {
+     *   await calendarHandler.new_event(eventPayload);
+     *   console.log('Event created successfully.');
+     * } catch (error) {
+     *   console.error(error.message);
+     * }
      */
     async new_event(payload) {
         const endpoint = this.#server_address + 'events/';
@@ -54,8 +98,23 @@ module.exports = class CalendarHandler {
     }
 
     /**
-     * Remove event.
-     * @param {any} id id of the desired event.
+     * Asynchronously removes an event from the WordPress Tribe Events API based on the provided ID.
+     *
+     * @async
+     * @param {string} id - The ID of the event to be removed.
+     * @returns {Promise<void>} A promise that resolves when the event removal is successful.
+     * @throws {Error} If an error occurs during the execution of the method.
+     *
+     * @example
+     * const calendarHandler = new CalendarHandler(serverAddress, headers);
+     * const eventIdToRemove = 'eventID';
+     *
+     * try {
+     *   await calendarHandler.remove_event(eventIdToRemove);
+     *   console.log('Event removed successfully.');
+     * } catch (error) {
+     *   console.error(error.message);
+     * }
      */
     async remove_event(id) {
         const endpoint = this.#server_address + 'events/' + id;
--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
-const CalendarHandler = require('./calendar-handler');
-
-
-async function main() {
-  const payload = {
-    id: 123,
-    title: 'Your Event Title',
-    start_date: '2023-11-24T10:00:00',
-    end_date: '2023-11-24T12:00:00'
-  };
-
-  const wordpressUser = 'vue_js';
-  const wordpressPassword = 'rXhW lh6q wuuv d3C5 IKrX YMkI';
-  const wordpressCredentials = `${wordpressUser}:${wordpressPassword}`;
-  const wordpressToken = Buffer.from(wordpressCredentials).toString('base64')
-  const wordpressHeader = { 'Authorization': `Basic ${wordpressToken}` };
-
-  const ch = new CalendarHandler('https://dev.htlweiz.at', wordpressHeader);
-}
+async function main() { }
 
 if (require.main === module) {
   main();
