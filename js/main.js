c8b3871c252824c93c3af91f2fc48b4970af3682
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2023-11-27 08:23:08 +0100

    added wp-api-handler

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-async function main() { }
+const WPApiHandler = require('./wp-api-handler')
+
+
+async function main() {
+  const wpa = WPApiHandler()
+}
 
 if (require.main === module) {
   main();
