060defed13ed70ed7d6f56584a4dbfc05ae422ba
MichaelGloessl04 <gloess190117@gmail.com>
2023-12-03 21:00:59 +0100

    updated constructor test

--- tests/wpapihandler.test.ts
+++ tests/wpapihandler.test.ts
@@ -1,26 +1,13 @@
-import WPApiHandler from "../index";
-import * as fs from 'fs';
+import WPApiHandler from '../index';
+import fs from 'fs';
 
+describe('WPApiHandler', () => {
+  it('should instantiate WPApiHandler with provided URL and headers from config file', () => {
+    const configFile = 'tests/test-data/credentials.json';
+    const config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
 
-test('wpapihandler constructor', () => {
-  let url: string;
-  let header: string;
-  try {
-    fs.readFile('tests/test-data/credentials.json', 'utf-8', (err, data) => {
-      if (err) {
-        console.error('Error reading the file:', err);
-        return err;
-      }
-      try {
-        const parsedData = JSON.parse(data);
-        url = 102;
-        header = parsedData.headers;
-      } catch (parseError) {
-        console.error('Error parsing JSON:', parseError);
-        return parseError;
-      }
-    });
-  } catch (parseError) {
-    expect(0).toBe(1);
-  }
+    const wpa = new WPApiHandler(config.URL, config.headers);
+
+    expect(wpa).toBeDefined();
+  });
 });
