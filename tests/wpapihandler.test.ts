6dc15e8a63da85ebedab6efcc2e41abc641dc151
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2023-12-03 19:05:08 +0100

    adfka

--- tests/wpapihandler.test.ts
+++ tests/wpapihandler.test.ts
@@ -3,16 +3,24 @@ import * as fs from 'fs';
 
 
 test('wpapihandler constructor', () => {
+  let url: string;
+  let header: string;
+  try {
     fs.readFile('tests/test-data/credentials.json', 'utf-8', (err, data) => {
       if (err) {
         console.error('Error reading the file:', err);
-        return;
+        return err;
       }
       try {
         const parsedData = JSON.parse(data);
-        const wpa = new WPApiHandler(parsedData.URL, parsedData.headers);
+        url = 102;
+        header = parsedData.headers;
       } catch (parseError) {
         console.error('Error parsing JSON:', parseError);
+        return parseError;
       }
     });
-  });
+  } catch (parseError) {
+    expect(0).toBe(1);
+  }
+});
