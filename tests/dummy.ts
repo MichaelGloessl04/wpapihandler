b8ff82c7390354133aa135075f66d7d7f6e0aec5
MichaelGloessl04 <gloess190117@gmail.com>
2023-12-11 14:06:14 +0100

    new testing system

--- tests/dummy.ts
+++ tests/dummy.ts
@@ -1,11 +1,3 @@
-export function dummy_success(): boolean {
+export function dummy(): boolean {
   return true;
-}
-
-export function dummy_fail(): boolean {
-  return false;
-}
-
-export function dummy_error(): boolean {
-  throw new Error('This is an expected Error!');
 }
--- tests/test-runnner.ts
+++ tests/test-runnner.ts
@@ -1,12 +1,12 @@
 import chalk from "chalk";
 
-import { dummy_success, dummy_fail, dummy_error } from "./dummy";
+import { dummy } from "./dummy";
 import { test_wpa_init } from "./test_wpapihandler";
+import { AssertionError } from "assert";
 
 
 const tests: (() => boolean)[] = [
-    dummy_success,
-    dummy_fail,
+    dummy,
     test_wpa_init
 ];
 
@@ -15,16 +15,16 @@ for (const test of tests) {
     try {        
         console.log(`Executing test: ${test.name}`);
         if (test()) {
-            console.log(chalk.green(`Function ${test.name} executed successfully.\n`));
+            console.log(chalk.green(`${test.name} executed successfully.\n`));
         } else {
-            throw new Error(chalk.yellow(`Function ${test.name} failed.\n`));
+            throw new AssertionError({message: chalk.yellow(`${test.name} failed.\n`)});
         }
     } catch (error: any) {
-        if (error.message == `Function ${test.name} failed.\n`) {
+        if (error.message == `${test.name} failed.\n`) {
             throw error;
         } else {
             console.error(
-                chalk.red(`Function ${test.name} failed with error:`, error, "\n")
+                chalk.red(`${test.name} failed with error:`, error.message, "\n")
             );
             throw error;
         }
