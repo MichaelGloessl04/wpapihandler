8528beeb71ab7bbcc9dedd090875d26014648678
MichaelGloessl04 <gloess190117@gmail.com>
2024-01-13 00:12:19 +0100

    new docs

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslIKD3AsyPRIzEvJSS1S0lEqSCzJAIom5yQWF6cW6yPL6mWU5OYAlWRn5qUoWRkaWdTqwE3xSE1MSS0qRhhQUlkA1A4VRtVpZGBpbmhqhKQ7IL+4BF0rSAy7vlgAAbH4470AAAA="
